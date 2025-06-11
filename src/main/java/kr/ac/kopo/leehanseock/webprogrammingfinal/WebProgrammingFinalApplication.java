46b00e43fbd0fc100b8ef26be039c8e0912da664
leehanseock <leehanseock@gmail.com>
2025-06-11 16:17:22 +0900

    initial commit

---
+++ src/main/java/kr/ac/kopo/leehanseock/webprogrammingfinal/WebProgrammingFinalApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.leehanseock.webprogrammingfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProgrammingFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebProgrammingFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kopo/leehanseock/webprogrammingfinal/WebProgrammingFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.leehanseock.webprogrammingfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProgrammingFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
